d440c9813d188218b30336360bf5fe8d911529ca
Eswar Maguluri <eswarmaguluri02@gmail.com>
2025-12-19 19:17:03 +0530

    Fix media handling using Cloudinary (Django 4.2+)

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('eswar/', views.eswar_redirect, name='eswar'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+    path('projects/<slug:slug>/', views.project_detail, name='project_detail'),
+]
